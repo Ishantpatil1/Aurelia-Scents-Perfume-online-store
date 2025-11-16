0950fdb7eb983e0f7b62b469efe68e8fbd9e1c97
Ishant Patil <Patilishant97@gmail.com>
2025-11-16 11:36:07 +0530

    update message

--- backend/seed.js
+++ backend/seed.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Product = require('./models/Product');
 const Review = require('./models/Review');
 const User = require('./models/User');
